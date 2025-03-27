86a1437f278e13ad5266970d79a3f9f1493e12ab
Zaitsev Georgiy <57594615+Orijuke@users.noreply.github.com>
2025-03-27 13:20:57 +0300

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -4,17 +4,52 @@ class StorageService {
     this.BOOKINGS_KEY = 'laundryBookings';
   }
 
-  getUser() {
-    const userData = localStorage.getItem(this.USER_KEY);
-    return userData ? JSON.parse(userData) : null;
+  // Генерация читаемого ID (пример: "user-5a3b2c")
+  generateReadableId() {
+    const randomPart = Math.random().toString(36).substr(2, 6);
+    return `user-${randomPart}`;
   }
 
-  saveUser(user) {
-    if (!user.id) {
-      user.id = this.generateId();
+  // Сохраняем пользователя с проверкой на существующий ID
+  saveUser(userData) {
+    let user = this.getUser();
+    if (!user || !user.id) {
+      user = { ...userData, id: this.generateReadableId() };
+    } else {
+      user = { ...user, ...userData };
     }
     localStorage.setItem(this.USER_KEY, JSON.stringify(user));
     return user;
+  }
+
+  // Получаем текущего пользователя
+  getUser() {
+    const data = localStorage.getItem(this.USER_KEY);
+    return data ? JSON.parse(data) : null;
+  }
+
+  // Обновляем все брони пользователя при изменении профиля
+  updateUserBookings(userId, updates) {
+    const allBookings = this.getBookings();
+    let changed = false;
+
+    for (const date in allBookings) {
+      for (const time in allBookings[date]) {
+        for (const machine in allBookings[date][time]) {
+          if (allBookings[date][time][machine].userId === userId) {
+            allBookings[date][time][machine] = { 
+              ...allBookings[date][time][machine], 
+              ...updates 
+            };
+            changed = true;
+          }
+        }
+      }
+    }
+
+    if (changed) {
+      localStorage.setItem(this.BOOKINGS_KEY, JSON.stringify(allBookings));
+    }
   }
 
   getBookings() {
